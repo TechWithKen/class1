f81d0a0580c426b088694b419cd4c2f6315db55a
Kehinde Ajasa <ti664469@gmail.com>
2024-06-24 15:39:04 +0100

    added file for assignment submission

---
+++ assignments.py
@@ -0,0 +1,26 @@
+"""
+classwork:
+given the following about a trapezium, 
+a = 6
+b = 10
+h = 8 and area = 1/2*(a+b)*h
+write  python code using variable declarations, operators
+and output statements to calculate the area of the 
+trapezium
+"""
+"""
+b = 5
+h = 30
+area_of_triangle = 1/2 * (b*h)
+write a python code to calcualte the area of the triangle
+"""
+
+_day = "i go make am"
+_day = "comming"
+b = 5
+B = 7
+h = 90
+area = 1/2 * (b*h)
+print(B)
+print(b)
+print(area)
